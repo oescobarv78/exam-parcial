27016f5c9085477d1c6520cd6b3eeee5c59c61a5
omar_ <omar_@GNOSIS-BRAIN>
2026-04-25 12:27:34 -0500

    Pregunta 1 completa: Modelos, Restricciones y Seed

---
+++ Data/Migrations/20260425172655_FinalizaPregunta1.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace exam_parcial.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class FinalizaPregunta1 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
